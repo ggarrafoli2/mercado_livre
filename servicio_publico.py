ead0ef49262cf0ec7cf90483fb716fa5eddbdee2
ggarrafoli2 <147544941+ggarrafoli2@users.noreply.github.com>
2023-10-11 15:10:31 -0300

    Update and rename servicio_publico.json to servicio_publico.py

---
+++ servicio_publico.py
@@ -0,0 +1,41 @@
+import requests
+import pandas as pd
+
+from pandas import json_normalize
+
+
+
+prods = ['chromecast','apple tv','alexa']
+def get_chrom(prod):
+#"""
+#Retorna uma lista de chromecast do Mercado Livre.
+
+#Args:
+#None
+
+#Returns:
+#Uma lista de chrom.
+#"""
+
+  url = "https://api.mercadolibre.com/sites/MLA/search?q=%s&limit=50#json" %prod
+  print(url)
+  response = requests.get(url)
+  if response.status_code == 200:
+    return response.json()["results"]
+  else:
+    raise Exception(response.status_code)
+
+
+if __name__ == "__main__":
+  df_=pd.DataFrame()
+
+  for nomes in prods:
+    produtos_ = get_chrom(nomes)
+    for produto in produtos_:
+      #print(produto)
+      #print(type(produto))
+      #df=json_normalize(produto["id"])
+      df=pd.DataFrame([produto])
+      df_=pd.concat([df_,df])
+  print(df_)    
+  print(df_.columns)
